3b1313c9293bbceebdc23f66ca5156dbf952771d
Asim Salimli <asimsalimli@Asims-MacBook-Pro.local>
2019-12-11 00:23:44 -0500

    message

---
+++ myjavascript.js
@@ -0,0 +1,24 @@
+var one = 'My name is Lala';
+
+
+
+//alert(one) ;
+
+//document.getElementById(message).innerHTML = one;
+
+//$('#test').text(one);
+//$('button').click(function () {
+//alert('stop');
+//});
+
+//console.log(4/0)
+
+let answer = +prompt("Are you 18 yr?", "Yes");
+
+console.log(typeof (answer));
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr)
+console.log(decr)
